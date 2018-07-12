f87f71565137c36125a81420c846b76de8b9ce71
xy97cornell <35710114+xy97cornell@users.noreply.github.com>
2018-07-12 10:59:15 -0400

    Changed stuff

--- EmonLib/examples/current_only/current_only.ino
+++ EmonLib/examples/current_only/current_only.ino
@@ -3,18 +3,26 @@
 #include "EmonLib.h"                   // Include Emon Library
 EnergyMonitor emon1;                   // Create an instance
 
+
+
 void setup()
 {  
-  Serial.begin(9600);
-  
+  Serial.begin(115200);
+  pinMode(8, OUTPUT);
   emon1.current(1, 111.1);             // Current: input pin, calibration.
+  int count = 0;
 }
 
 void loop()
 {
-  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
-  
-  Serial.print(Irms*230.0);	       // Apparent power
-  Serial.print(" ");
-  Serial.println(Irms);		       // Irms
+  digitalWrite(8, HIGH);
+  //double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  double reading = analogRead(1);
+    digitalWrite(8, LOW);
+  //Serial.print(Irms*120.0*.7071);	       // Apparent power
+  //Serial.print(" ");
+  //Serial.println(Irms);		       // Irms
+  Serial.println(reading);
+  //emon1.serialprint();
+
 }
