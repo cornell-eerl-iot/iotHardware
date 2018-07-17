d12ee2b85b66364e857365fde6f9e8af47e7f86c
xy97cornell <35710114+xy97cornell@users.noreply.github.com>
2018-07-16 22:59:55 -0400

    Changed some numbers

--- EmonLib/examples/current_only/current_only.ino
+++ EmonLib/examples/current_only/current_only.ino
@@ -9,7 +9,7 @@ void setup()
 {  
   Serial.begin(115200);
   pinMode(8, OUTPUT);
-  emon1.current(1, 300);             // Current: input pin, calibration.
+  emon1.current(1, 600.6);             // Current: input pin, calibration.
   int count = 0;
 }
 
@@ -19,8 +19,8 @@ void loop()
   double Irms = emon1.calcIrms(1480);  // Calculate Irms only
   //double reading = analogRead(1);
     //digitalWrite(8, LOW);
-  //Serial.print(Irms*120.0*.7071);	       // Apparent power
-  //Serial.print(" ");
+  Serial.print(Irms*116.8/.7071);	       // Apparent power
+  Serial.print(" ");
   Serial.println(Irms);		       // Irms
   //Serial.println(reading);
   //emon1.serialprint();
