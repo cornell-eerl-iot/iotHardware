992760d5e1484fa391187bca96f5657bf58d4a1c
xy97cornell <xy97@cornell.edu>
2018-07-23 22:07:30 -0400

    -a

--- Arduino_meter/Arduino_meter.ino
+++ Arduino_meter/Arduino_meter.ino
@@ -1,16 +1,25 @@
 
 
+V_DC_OFFSET = 2.5;
+I_DC_OFFSET = 2.5;
+
+//CT: 200A:0.33VAC
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(A1,INPUT);
-  pinMode(A2,INPUT);
+  pinMode(A0,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int value1 = analogRead(A1);
-  int value2 = analogRead(A2);
-  Serial.print(value1);
-  Serial.println(value2);
+  int current = analogRead(A1);
+  int voltage = analogRead(A0);
+  Serial.print(current);
+  Serial.print(" ");
+  Serial.println(voltage);
 }
--- EmonLib/examples/voltage_and_current/voltage_and_current.ino
+++ EmonLib/examples/voltage_and_current/voltage_and_current.ino
@@ -7,8 +7,8 @@ void setup()
 {  
   Serial.begin(9600);
   
-  emon1.voltage(2, 234.26, 1.7);  // Voltage: input pin, calibration, phase_shift
-  emon1.current(1, 111.1);       // Current: input pin, calibration.
+  emon1.voltage(0, 110, 1.7);  // Voltage: input pin, calibration, phase_shift
+  emon1.current(1, 600);       // Current: input pin, calibration.
 }
 
 void loop()
