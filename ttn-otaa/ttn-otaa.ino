1e37477c6c0c02616814b826eff71578ff5b776c
xy97cornell <xy97@cornell.edu>
2018-08-13 18:00:58 -0400

    a

--- ttn-otaa/ttn-otaa.ino
+++ ttn-otaa/ttn-otaa.ino
@@ -154,6 +154,8 @@ void onEvent (ev_t ev) {
             // during join, but because slow data rates change max TX
       // size, we don't use it in this example.
             LMIC_setLinkCheckMode(0);
+            LMIC_setDrTxpow(DR_SF7,14);
+            LMIC_selectSubBand(1);
             break;
         /*
         || This event is defined but not used in the code. No
