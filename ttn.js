7fc80390491d92314d7e475e7c5ffd5a5a9d55c3
Xiaoyu <xy97@cornell.edu>
2019-01-28 17:18:16 -0500

    -a

---
+++ ttn.js
@@ -0,0 +1,48 @@
+function Decoder(bytes, port) {
+    // Decode an uplink message from a buffer
+    // (array) of bytes to an object of fields.
+    var decoded = {};
+    var i = 0; //bytes left
+    var s = 0; //seconds
+    // decoded["_Connection Number"] = bytes[i++];
+    decoded["_Minutes"] = bytes[i++];
+    decoded["_Seconds"] = bytes[i++];
+    
+    while(i<bytes.length){
+      decoded[s+" Grid_Power-A-Real"] = bytes[i++]|(bytes[i++]<<8);
+      decoded[s+" Grid_Power-B-Real"] = bytes[i++]|(bytes[i++]<<8);
+      decoded[s+" Grid_Power-C-Real"] = bytes[i++]|(bytes[i++]<<8);
+      decoded[s+" Grid_Power-A-Reactive"] = bytes[i++]|(bytes[i++]<<8);
+      decoded[s+" Grid_Power-B-Reactive"] = bytes[i++]|(bytes[i++]<<8);
+      decoded[s++ +" Grid_Power-C-Reactive"] = bytes[i++]|(bytes[i++]<<8);
+    }
+  
+    return decoded;
+  }
+
+function Converter(decoded, port) {
+    // Merge, split or otherwise
+    // mutate decoded fields.
+    var converted = decoded;
+    
+    for(var key in decoded){
+      if(!(key=="_Connection Number"||key=="_Minutes"||key=="_Seconds")){
+        var sign = (converted[key]&0x8000)>>>15;
+        var expo = (converted[key]&0x7c00)>>>10;
+        var manti = converted[key]&0x3ff;
+        if(expo === 0 && manti === 0){
+          converted[key]=0;
+        }else if(expo==0x1F && manti === 0){
+          converted[key] = Infinity;
+        }else{
+          var e = expo-15;
+          var result = Math.pow(2,e);
+          result=result*(1+Math.pow(2,-10)*manti);
+          converted[key] = sign?-result:result;   
+        }
+      }
+    }
+    return converted;
+  }
+
+
